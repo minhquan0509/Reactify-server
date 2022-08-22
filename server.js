33bf6815a4e3f8c8660fa4545e0375f2f621bdd5
MinhQuan <minkkquan2@gmail.com>
2022-08-22 15:59:02 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) =>{
+    const refreshToken = req.body.refreshToken
+    const spotifyWebApi = new SpotifyWebApi({
+        redirectUri: process.env.REACT_APP_DEV_URL,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+        refreshToken
+    })
+
+    spotifyWebApi
+    .refreshAccessToken()
+    .then(
+        (data) => {
+          res.json({
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in,
+          })   
+        })
+    .catch((err) => {
+        res.sendStatus(400)
+    })      
+})
+
+app.post('/login', (req, res) =>{
+    const code = req.body.code
+    const spotifyWebApi = new SpotifyWebApi({
+        redirectUri: process.env.REACT_APP_DEV_URL,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET
+    })
+
+    spotifyWebApi.authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+        
+    })
+    .catch( (err) => {
+        console.log(err);
+        res.sendStatus(400)
+    })
+})
+
+app.listen(3001);
